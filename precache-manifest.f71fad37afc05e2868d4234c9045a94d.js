b9ad96b82cb1533c0d8ad883f56a0b35ff5e24b7
IvanDilberovic <dilberovic.ivan@gmail.com>
2019-07-27 20:26:07 +0200

    Updates

---
+++ precache-manifest.f71fad37afc05e2868d4234c9045a94d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b8a523ff8dacf1b3172cb83c5dea241",
+    "url": "/mnist_image_recognition/static/media/Logo.8b8a523f.png"
+  },
+  {
+    "revision": "9a3ffe323bd63e00c318",
+    "url": "/mnist_image_recognition/static/js/runtime~main.9a3ffe32.js"
+  },
+  {
+    "revision": "02b999c653ac1ba6f634",
+    "url": "/mnist_image_recognition/static/js/main.02b999c6.chunk.js"
+  },
+  {
+    "revision": "2f26dfaaee62285bd00f",
+    "url": "/mnist_image_recognition/static/js/1.2f26dfaa.chunk.js"
+  },
+  {
+    "revision": "02b999c653ac1ba6f634",
+    "url": "/mnist_image_recognition/static/css/main.9466304a.chunk.css"
+  },
+  {
+    "revision": "2f26dfaaee62285bd00f",
+    "url": "/mnist_image_recognition/static/css/1.e12ec38e.chunk.css"
+  },
+  {
+    "revision": "79729c1ce7a95c63f5bf6ea68dd3afeb",
+    "url": "/mnist_image_recognition/index.html"
+  }
+];
